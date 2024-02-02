286723af028ea2e7a3ac1353793ec00e70b30c11
Kajsa <kajsalisaasp@gmail.com>
2024-02-02 13:04:15 +0100

    Location component added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script>
   import CitiesList from './components/CitiesList.vue'
   import HelloWorld from './components/HelloWorld.vue'
+  import MainLayout from './components/MainLayout.vue'
+  import GetLocation from './components/GetLocation.vue'
 
   export default {
     components: {
       CitiesList,
-      HelloWorld
+      HelloWorld,
+      MainLayout,
+      GetLocation
     }
   }
 </script>
 
 <template>
+  <GetLocation></GetLocation>
+  <MainLayout></MainLayout>
   <HelloWorld msg="Hello World!" />
   <CitiesList />
 </template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import '../assets/weather.css'
 
 createApp(App).mount('#app')
